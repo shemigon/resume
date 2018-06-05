02819a605466ba832093d22b4df2751d2cf71ca7
Boris Shemigon <bshemigon@charterschoolsusa.com>
2018-06-05 12:00:11 -0400

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('resume-root h1')).getText();
+  }
+}
